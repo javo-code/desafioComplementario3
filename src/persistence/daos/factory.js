8cd6c6445a1eb9b52fbebbc36cfb1e4b0eee23da
Romero Javier <romerofj26@gmail.com>
2024-02-01 19:31:47 -0300

    mysql persistence added

--- src/persistence/daos/factory.js
+++ src/persistence/daos/factory.js
@@ -1,12 +1,17 @@
+//---------------------------------📌 FS
 import ProductFSDao from "./fileSystem/products.dao.js";
 import CartFSDao from "./fileSystem/carts.dao.js";
 import UserFSDao from "./fileSystem/users.dao.js";
 import TicketFSDao from "./fileSystem/ticket.dao.js";
-
+//---------------------------------📌 MONGO
 import ProductMongoDao from "./mongoDB/products/product.dao.js";
 import CartMongoDao from "./mongoDB/cart/cart.dao.js";
 import UserMongoDao from "./mongoDB/users/user.dao.js";
 import TicketMongoDao from "./mongoDB/ticket/ticket.dao.js";
+//---------------------------------📌 MYSQL
+import ProductDaoMySql from './mysql/product.dao.js';
+import UserDaoMySql from './mysql/user.dao.js';
+import { initMySqlDB } from './mysql/connection.js';
 
 import "dotenv/config"
 import { initMongoDB } from "../../config/connection.js";
@@ -34,7 +39,12 @@ switch (persistence) {
         ticket = new TicketMongoDao();
         console.log("📚 La PERSISTENCIA actual es => ", persistence);
         break;
-
+    case 'MYSQL':
+        await initMySqlDB();
+        userDao = new UserDaoMySql();
+        prodDao = new ProductDaoMySql();
+        console.log(persistence);
+        break;
     default:
         await initMongoDB();
         userDao = new ProductMongoDao();
